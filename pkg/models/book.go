2ea1151adbfcc8aa516289c12450a07173876d28
Mikepopup <box-need007@mail.ru>
2022-10-17 19:02:08 +0800

    created crud func in book.go

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -10,14 +10,38 @@ var db *gorm.DB
 
 type Book struct {
 	gorm.Model
-	Name string `gorm: ""json:"name"`
-	Author string `json: "author"`
+	Name        string `gorm: ""json:"name"`
+	Author      string `json: "author"`
 	Publication string `json: "publication"`
 }
 
 // Creating init function to connect with db
-func init(){
+func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById (Id int64) (*Book, *gorm.DB){
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
 }
